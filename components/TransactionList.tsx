ad72ed6bea00e3cec7d2acf7ed0e85aadbdd0f8d
ericstober <erictstober@gmail.com>
2024-08-20 08:33:25 -0500

    added TransactionItem component and deleteTransaction action

--- components/TransactionList.tsx
+++ components/TransactionList.tsx
@@ -1,5 +1,6 @@
 import { Transaction } from "@/types/Transaction";
 import getTransactions from "@/app/actions/getTransactions";
+import TransactionItem from "./TransactionItem";
 
 const TransactionList = async () => {
   const { transactions, error } = await getTransactions();
@@ -11,7 +12,7 @@ const TransactionList = async () => {
   return (
     <>
       <h3>History</h3>
-      <ul className='list'>{transactions && transactions.map((transaction: Transaction) => <p>{transaction.text}</p>)}</ul>
+      <ul className='list'>{transactions && transactions.map((transaction: Transaction) => <TransactionItem key={transaction.id} transaction={transaction} />)}</ul>
     </>
   );
 };
